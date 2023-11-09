7aa3ccfdf823b2cbf73faa4a3ecb8bfcd17edafe
jzhupan <jzhupan@gmail.com>
2023-11-09 16:52:53 -0500

    a Practice refresh on Components, Rendering Lists, Conditional Rendering, Handling Events, Managing State, and Passing Data via Props.

--- react-financial-dashboard/src/App.tsx
+++ react-financial-dashboard/src/App.tsx
@@ -1,10 +1,20 @@
 import React from "react";
-import Message from "./Message";
+import ListGroup from "./components/ListGroup";
 
 const App = () => {
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
   return (
     <div>
-      <Message />
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
     </div>
   );
 };
